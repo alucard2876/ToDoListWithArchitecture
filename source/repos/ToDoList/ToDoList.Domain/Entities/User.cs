42e39ce0ca9ec730f74e7f64284ae346b965cd71
Vlad <youfillingtodontyou@gmail.com>
2020-08-08 19:47:49 +0300

    Add current model

--- source/repos/ToDoList/ToDoList.Domain/Entities/User.cs
+++ source/repos/ToDoList/ToDoList.Domain/Entities/User.cs
@@ -4,7 +4,30 @@ using System.Text;
 
 namespace ToDoList.Domain.Entities
 {
-    class User
+    public class User : EntityBase
     {
+        public string UserName { get; private set; }
+        public string Password { get; private set; }
+        protected User()
+        {
+
+        }
+        public User(string userName, string password)
+        {
+            if (string.IsNullOrEmpty(userName)) throw new ArgumentNullException(nameof(userName));
+            if (string.IsNullOrEmpty(password)) throw new ArgumentNullException(nameof(password));
+            UserName = userName;
+            Password = password;
+        }
+        public void UpdateUserName(string userName)
+        {
+            if (string.IsNullOrEmpty(userName)) throw new ArgumentNullException(nameof(userName));
+            UserName = userName;
+        }
+        public void UpdatePassword(string password)
+        {
+            if (string.IsNullOrEmpty(password)) throw new ArgumentNullException(nameof(password));
+            Password = password;
+        }
     }
 }
